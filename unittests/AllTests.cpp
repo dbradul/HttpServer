b9d1ffe897111b75f100694cb772ee6c5bc508cb
onemoresunday <onemoresunday@gmail.com>
2014-07-17 15:23:13 +0300

    unittests renamed

---
+++ unittests/AllTests.cpp
@@ -0,0 +1,79 @@
+#include <TestHarness.h>
+#include <builder/Templater.h>
+#include <builder/File.h>
+
+namespace
+{
+   struct Example
+   {
+         int value()
+         {
+            return 42;
+         }
+   };
+
+   struct ExampleFixture
+   {
+     Example e_;
+   };
+} // namespace
+
+TEST_F(ExampleFixture, when_doing_this_that_will_happen_as_a_result)
+{
+  // Arrange
+  // Act
+  // Assert
+  ASSERT_EQ(42, e_.value());
+}
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(Example, simple_test_of_something_that_should_result_in_something)
+{
+  // Arrange
+  Example e;
+  // Act
+  int value = e.value();
+  // Assert
+  ASSERT_EQ(42, value);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(Templater, simple_test)
+{
+  // Arrange
+  Templater templater;
+  Templater templater2(Templater::TEMPLATE_ROOT_LAYOUT);
+  // Act
+  // Assert
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(File, simple_test)
+{
+   // Arrange
+   File file_a;
+   File file_b;
+
+   File dir_a;
+   File dir_b;
+
+   file_a.name = "aaa";
+   file_b.name = "bbb";
+
+   dir_a.name  = "ccc";
+   dir_a.isDir = true;
+   dir_b.name  = "ddd";
+   dir_b.isDir = true;
+
+   // Act
+   // Assert
+   ASSERT_TRUE(file_a < file_b);
+   ASSERT_TRUE(dir_a  < dir_b);
+
+   ASSERT_TRUE(dir_a < file_a);
+   ASSERT_TRUE(dir_a < file_b);
+
+   ASSERT_TRUE(dir_b < file_a);
+   ASSERT_TRUE(dir_b < file_b);
+}
---
+++ unittests/subdir.mk
@@ -0,0 +1,17 @@
+################################################################################
+# HttpServer
+# DB, 2014
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+./unittests/AllTests.cpp
+
+OBJS += \
+./Debug/unittests/AllTests.o
+
+# Each subdirectory must supply rules for building sources it contributes
+Debug/unittests/%.o: ./unittests/%.cpp
+	$(CC) $(CFLAGS) "$<"
+
+
